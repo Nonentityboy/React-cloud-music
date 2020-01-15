a4891b7c71c04929adb7a0781e9f0eca98a128e3
nonentityboy <1138580559@qq.com>
2020-01-15 22:00:05 +0800

    fix eslint warnning

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -7,14 +7,14 @@ import {
 } from './style';
 import { NavLink } from 'react-router-dom';// 利用 NavLink 组件进行路由跳转
 
-function Home (props){
+function Home(props) {
   const { route } = props;
 
   return (
     <div>
       <Top>
         <span className="iconfont menu">&#xe65c;</span>
-        <span className="title">Web App</span>
+        <span className="title">网易云音乐</span>
         <span className="iconfont search">&#xe62b;</span>
       </Top>
       <Tab>
@@ -22,9 +22,9 @@ function Home (props){
         <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
         <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
       </Tab>
-      { renderRoutes (route.routes) }
+      {renderRoutes(route.routes)}
     </div>
   );
 }
- 
-export default React.memo (Home);
+
+export default React.memo(Home);
