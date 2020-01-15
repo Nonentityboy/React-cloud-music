35b6f997cca97d72f19838062425bceceaf6067c
nonentityboy <1138580559@qq.com>
2020-01-15 21:45:01 +0800

    add home-top

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,16 +1,30 @@
-//src/application/Home/index.js
 import React from 'react';
 import { renderRoutes } from "react-router-config";
+import {
+  Top,
+  Tab,
+  TabItem,
+} from './style';
+import { NavLink } from 'react-router-dom';// 利用 NavLink 组件进行路由跳转
 
-function Home (props) {
-    const { route } = props;
+function Home (props){
+  const { route } = props;
 
-    return (
-      <div>
-        <div>Home</div>
-        { renderRoutes (route.routes) }
-      </div>
-    )
+  return (
+    <div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">Web App</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+        <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+        <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+      </Tab>
+      { renderRoutes (route.routes) }
+    </div>
+  );
 }
-
-export default React.memo(Home);
+ 
+export default React.memo (Home);
