cb9862b54dcd693089bd449bfba57ca271e4271e
nonentityboy <1138580559@qq.com>
2020-02-28 13:06:18 +0800

    add slide

--- src/components/slider/style.js
+++ src/components/slider/style.js
@@ -10,8 +10,8 @@ export const SliderContainer = styled.div`
   background: white;
   .before {
     position: absolute;
-    top: 0;
-    height: 60%;
+    top: -300px;
+    height: 400px;
     width: 100%;
     background: ${style["theme-color"]};
   }
