f960b1cc392e783300920d2ac7741e26c3373f1f
nonentityboy <1138580559@qq.com>
2020-02-29 09:04:15 +0800

    update

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,10 +1,18 @@
 export const getCount = (count) => {
-    if (count < 0) return;
-    if (count < 10000) {
-      return count;
-    } else if (Math.floor (count / 10000) < 10000) {
-      return Math.floor (count/1000)/10 + "万";
-    } else  {
-      return Math.floor (count / 10000000)/ 10 + "亿";
+  if (count < 0) return;
+  if (count < 10000) {
+    return count;
+  } else if (Math.floor(count / 10000) < 10000) {
+    return Math.floor(count / 1000) / 10 + "万";
+  } else {
+    return Math.floor(count / 10000000) / 10 + "亿";
+  }
+}
+
+export const filterIndex = rankList => {
+  for (let i = 0; i < rankList.length - 1; i++) {
+    if (rankList[i].tracks.length && !rankList[i + 1].tracks.length) {
+      return i + 1;
     }
   }
+};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
 import { reducer as singersReducer } from '../application/Singers/store/index';
+import { reducer as rankReducer } from '../application/Rank/store/index';
 
-export default combineReducers({
+export default combineReducers ({
+  // 之后开发具体功能模块的时候添加 reducer
   recommend: recommendReducer,
-  singers: singersReducer,
+  singers: singersReducer ,
+  rank: rankReducer
 });
